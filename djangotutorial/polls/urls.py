0fe70795cea0f6feed0cbfaf61fd9d2d75ab1907
tobias <jshlamovitzalfonso@alumno.huergo.edu.ar>
2026-09-03 15:48:40 -0300

    aaaa

---
+++ djangotutorial/polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+]
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
---
+++ djangotutorial/polls/views.py
@@ -0,0 +1,6 @@
+import django
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
